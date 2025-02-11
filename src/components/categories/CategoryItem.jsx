46f35837d1e510e034b5140219dbf02f4b930a14
herringvoices <nalherring@gmail.com>
2025-02-11 15:47:37 -0600

    Added editing functionality to categories component

--- src/components/categories/CategoryItem.jsx
+++ src/components/categories/CategoryItem.jsx
@@ -1,20 +1,67 @@
-import { Button } from "reactstrap";
-import { deleteCategory } from "../../managers/categoryManager";
+import { Button, Input } from "reactstrap";
+import { deleteCategory, updateCategory } from "../../managers/categoryManager";
+import { useState } from "react";
 
 function CategoryItem({ category, getAndSetCategories }) {
+  const [toggleEdit, setToggleEdit] = useState(false);
+  const [editCategory, setEditCategory] = useState(category?.name);
+
   const handleDelete = async (e) => {
     e.preventDefault();
     await deleteCategory(category.id);
     await getAndSetCategories();
   };
 
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    const updatedCategory = { ...category, name: editCategory };
+    await updateCategory(updatedCategory);
+    await getAndSetCategories();
+    setToggleEdit(false);
+    setEditCategory(category.name);
+  };
+
+  const handleChange = (e) => {
+    setEditCategory(e.target.value);
+  };
+
   return (
     <tr>
       <td style={{ color: "var(--grey)" }}>
         <div className="d-flex justify-content-between align-items-center">
-          <span>{category.name}</span>
+          {toggleEdit ? (
+            <Input
+              onChange={handleChange}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleUpdate(e);
+                }
+              }}
+              className="w-50"
+              type="text"
+              value={editCategory || ""}
+              autoFocus
+            />
+          ) : (
+            <span>{category.name}</span>
+          )}
           <div>
-            <Button className="mx-1">Edit</Button>
+            {toggleEdit ? (
+              <Button onClick={handleUpdate} className="mx-1 save-btn">
+                Save
+              </Button>
+            ) : (
+              <Button
+                onClick={() => {
+                  setToggleEdit(true);
+                  setEditCategory(category?.name);
+                }}
+                className="mx-1"
+              >
+                Edit
+              </Button>
+            )}
+
             <Button className="mx-1 delete-btn" onClick={handleDelete}>
               Delete
             </Button>
