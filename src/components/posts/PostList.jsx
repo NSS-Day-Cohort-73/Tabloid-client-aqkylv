464860f409a544a6629df63cd841d3c88c782bba
mleelaw <mleelaw123@gmail.com>
2025-02-11 12:43:42 -0600

    issue #3 - added PostList view on Explore Tab

--- src/components/posts/PostList.jsx
+++ src/components/posts/PostList.jsx
@@ -1,33 +1,39 @@
 import { useEffect, useState } from "react";
-import { Card, CardBody, CardTitle, CardText } from "reactstrap";
+import { Card, CardBody, CardTitle, CardText, Container } from "reactstrap";
 import { getAllPosts } from "../../managers/postManager";
 
-export default function PostList({ loggedInUser }) {
+export default function PostList() {
     const [posts, setPosts] = useState([]);
     
-        function getAndSetPosts() {
-          getAllPosts().then(setPosts);   
-        }
+    function getAndSetPosts() {
+        getAllPosts().then(setPosts);   
+    }
     
-        useEffect(() => {
-            getAndSetPosts();
-        }, []);
+    useEffect(() => {
+        getAndSetPosts();
+    }, []);
 
-  return (
-    <>
-      <h4>PostLand</h4>
-      
-      {posts.map((p) => (
-        <Card outline color="sucsess" key={p.id} style={{ marginBottom: "4px" }}>
-          <CardBody>
-            <CardTitle tag="h5">{p.title}</CardTitle>
-            <CardText>
-              <div>Author: {p.author.fullName}</div>
-              <div>Category: {p.category.name}</div>
-            </CardText>
-          </CardBody>
-        </Card>
-      ))}
-    </>
-  );
+    return (
+        <Container>
+            <h4>PostLand</h4>
+            
+            {posts.map((p) => (
+                <Card 
+                    key={p.id} 
+                    outline 
+                    color="success" 
+                    style={{ marginBottom: "10px" }}
+                >
+                    <CardBody>
+                        <CardTitle tag="h5">{p.title}</CardTitle>
+                        <CardText tag="div">
+                            Author: {p.author.fullName}
+                            <br />
+                            Category: {p.category.name}
+                        </CardText>
+                    </CardBody>
+                </Card>
+            ))}
+        </Container>
+    );
 }
