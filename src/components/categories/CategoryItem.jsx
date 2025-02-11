c4b5f0a489f83685108ace20b8e8bc2e9b9e6dde
herringvoices <nalherring@gmail.com>
2025-02-11 14:43:45 -0600

    Added delete functionality.

--- src/components/categories/CategoryItem.jsx
+++ src/components/categories/CategoryItem.jsx
@@ -1,16 +1,25 @@
-import { Button, Col, Row } from "reactstrap";
+import { Button } from "reactstrap";
+import { deleteCategory } from "../../managers/categoryManager";
 
-function CategoryItem({ category, index }) {
+function CategoryItem({ category, getAndSetCategories }) {
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    await deleteCategory(category.id);
+    await getAndSetCategories();
+  };
+
   return (
     <tr>
       <td style={{ color: "var(--grey)" }}>
-        <Row>
-          <Col className="my-auto text-start">{category.name}</Col>
-          <Col className="text-end">
+        <div className="d-flex justify-content-between align-items-center">
+          <span>{category.name}</span>
+          <div>
             <Button className="mx-1">Edit</Button>
-            <Button className="mx-1 delete-btn">Delete</Button>
-          </Col>
-        </Row>
+            <Button className="mx-1 delete-btn" onClick={handleDelete}>
+              Delete
+            </Button>
+          </div>
+        </div>
       </td>
     </tr>
   );
