fca163cfd884c11828d55c0b235915217898f2cc
herringvoices <nalherring@gmail.com>
2025-02-11 11:40:50 -0600

    Added controller for getting category by id and posting category

--- src/components/categories/CategoryList.jsx
+++ src/components/categories/CategoryList.jsx
@@ -1,29 +1,29 @@
 import { useEffect, useState } from "react";
 import { getAllCategories } from "../../managers/categoryManager";
-import { Container } from "reactstrap";
+import { Button, Container, Form, Input } from "reactstrap";
 import CategoryItem from "./CategoryItem";
 
-function CategoryList () {
+function CategoryList() {
+  const [categories, setCategories] = useState([]);
 
-    const [categories, setCategories] = useState([]);
+  function getAndSetCategories() {
+    getAllCategories().then(setCategories);
+  }
 
-    function getAndSetCategories() {
-      getAllCategories().then(setCategories);   
-    }
+  useEffect(() => {
+    getAndSetCategories();
+  }, []);
 
-    useEffect(() => {
-        getAndSetCategories();
-    }, []);
-
-    return (
-      <Container className="mx-auto">
-        {categories.map((category) => (
-          <CategoryItem
-            key={category.id}
-            category={category}
-          />
-        ))}
-      </Container>
-    );
+  return (
+    <Container className="mx-auto">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+      <Form>
+        <Input type="text" placeholder="Add a category" />
+        <Button>Add</Button>
+      </Form>
+    </Container>
+  );
 }
 export default CategoryList;
