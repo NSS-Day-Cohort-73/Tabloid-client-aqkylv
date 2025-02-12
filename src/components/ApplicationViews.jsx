83cdbe054dc19a7bbff0b3eed96027ef31aad6fb
herringvoices <nalherring@gmail.com>
2025-02-12 14:50:57 -0600

    Added routing and styling for comments.

--- src/components/ApplicationViews.jsx
+++ src/components/ApplicationViews.jsx
@@ -7,6 +7,7 @@ import UserProfileDetails from "./userprofiles/UserProfileDetails";
 import NavBar from "./NavBar"; // Ensure you import your NavBar component
 import CategoryList from "./categories/CategoryList";
 import PostList from "./posts/PostList";
+import CommentsList from "./comments/CommentsList";
 
 export default function ApplicationViews({ loggedInUser, setLoggedInUser }) {
   return (
@@ -52,6 +53,14 @@ export default function ApplicationViews({ loggedInUser, setLoggedInUser }) {
           element={
             <AuthorizedRoute loggedInUser={loggedInUser}>
               <p>My Posts</p>
+            </AuthorizedRoute>
+          }
+        />
+        <Route
+          path="comments/:postId"
+          element={
+            <AuthorizedRoute loggedInUser={loggedInUser}>
+              <CommentsList />
             </AuthorizedRoute>
           }
         />
