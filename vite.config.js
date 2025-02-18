152221e2421f91f2ead1ec78daebf30696434ce5
herringvoices <nalherring@gmail.com>
2025-02-18 13:47:36 -0600

    Added capability to add new reactions and moved subscribe button to a badge next to author name.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
   return {
+    
     server: {
       open: true,
       proxy: {
@@ -13,6 +14,7 @@ export default defineConfig(() => {
         },
       },
     },
+    
     build: {
       outDir: "build",
     },
